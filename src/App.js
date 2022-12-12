518169faa0f6ab9fae174fff23c26da593ec60da
Piyush Jain <piyush200997jain@gmail.com>
2022-12-12 17:54:25 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,82 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import SingleCard from "./SingleCard";
+import "./App.css";
+
+const cardImages = [
+  { src: "./img/potion-1.png", matched: false },
+  { src: "./img/helmet-1.png", matched: false },
+  { src: "./img/sword-1.png", matched: false },
+  { src: "./img/ring-1.png", matched: false },
+  { src: "./img/scroll-1.png", matched: false },
+  { src: "./img/shield-1.png", matched: false },
+];
 
 function App() {
+  const [cards, setCards] = useState([]);
+  const [turn, setTurns] = useState(0);
+  const [choiceOne, setChoiceOne] = useState(null);
+  const [choiceTwo, setChoiceTwo] = useState(null);
+  const [disabled, setDisabled] = useState(false);
+
+  // shuffle cards
+  const shuffleCards = () => {
+    const shuffledCards = [...cardImages, ...cardImages].sort(() => Math.random() - 0.5).map((card) => ({ ...card, id: Math.random() }));
+    setCards(shuffledCards);
+    setTurns(0);
+    setChoiceOne(null);
+    setChoiceTwo(null);
+  };
+  const handleChoice = (card) => {
+    choiceOne ? setChoiceTwo(card) : setChoiceOne(card);
+  };
+  //compare that 2 cards are equal or not used useEffect as write it sequentially under handle choice a setChoiceTwo is async function.
+  // dependency array contains only choiceTwo because accoring to handle click function choiceTwo can only be selected after choiceOne.
+  useEffect(() => {
+    if (choiceTwo) {
+      setDisabled(true);
+      if (choiceOne.src === choiceTwo.src && choiceOne.id !== choiceTwo.id) {
+        // updating card in cards state(array) which are selected in choiceOne and choiceTwo
+        setCards((CardsArr) => {
+          return CardsArr.map((card) => {
+            if (card.id === choiceOne.id || card.id === choiceTwo.id) {
+              return { ...card, matched: true };
+            }
+            return card;
+          });
+        });
+      }
+      setTimeout(() => resetTurn(), 1000);
+    } else {
+      resetTurn();
+    }
+  }, [choiceTwo]);
+
+  useEffect(() => {
+    shuffleCards();
+  }, []);
+  const resetTurn = () => {
+    setChoiceOne(null);
+    setChoiceTwo(null);
+    setDisabled(false);
+    setTurns((t) => t + 1);
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Magic Match</h1>
+      <button onClick={shuffleCards}>New Game</button>
+      <div
+        className="card-grid"
+        style={{
+          marginTop: "40px",
+          display: "grid",
+          gridGap: "20px",
+          gridTemplateColumns: `repeat(${4},1fr)`,
+        }}>
+        {cards.map((card) => {
+          return <SingleCard card={card} handleChoice={handleChoice} key={card.id} flipped={card === choiceOne || card === choiceTwo || card.matched} disabled={disabled} />;
+        })}
+      </div>
+      <p>Turns:{turn}</p>
     </div>
   );
 }
---
+++ src/SingleCard.js
@@ -0,0 +1,17 @@
+import "./SingleCard.css";
+function SingleCard({ card, handleChoice, flipped, disabled }) {
+  function handleClick() {
+    if (!disabled) {
+      handleChoice(card);
+    }
+  }
+  return (
+    <div className="card">
+      <div className={flipped ? "flipped" : ""}>
+        <img src={card.src} className="front" alt="card front" />
+        <img src="./img/cover.png" className="back" alt="card back" onClick={handleClick} />
+      </div>
+    </div>
+  );
+}
+export default SingleCard;
